ff5aeb54666429acf9a03ea0900d89aaca5ce578
cgty <c.akinbaser@hotmail.com>
2022-10-21 14:15:36 +0200

    Added Logger and Gateway connection between AuthService and Gateway

--- auth-service/src/main/java/com/cbaser/authservice/controllers/AuthController.java
+++ auth-service/src/main/java/com/cbaser/authservice/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.cbaser.authservice.controllers;
 import com.cbaser.authservice.entities.AuthRequest;
 import com.cbaser.authservice.entities.AuthResponse;
 import com.cbaser.authservice.service.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/auth")
 public class AuthController {
 
+    private final Logger logger = LoggerFactory.getLogger(AuthController.class);
     private final AuthService authService;
 
     @Autowired
@@ -23,10 +26,12 @@ public class AuthController {
 
     @PostMapping(value = "/register")
     public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest authRequest) {
+        logger.info("Register is called");
         return ResponseEntity.ok(authService.register(authRequest));
     }
     @PostMapping
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest){
+        logger.info("Login is called");
         return ResponseEntity.ok(authService.login(authRequest));
     }
 }
